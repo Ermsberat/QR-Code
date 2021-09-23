e94ff423b10c4b1829f32e4c4ef8507993b34eb6
Berat <91216150+Ermsberat@users.noreply.github.com>
2021-09-23 19:55:25 +0300

    Add files via upload

---
+++ QR Code.py
@@ -0,0 +1,26 @@
+# pip istall ile qrcode ve Image kütüphanesini indirmeyi unutmayınız.
+# Do not forget to download the qrcode and Image library with pip install.
+import qrcode
+
+print("""
+
+this QR CODE was created by Berat
+
+github.com/Ermsberat
+
+""")
+
+print("""
+Lütfen Veriyi Giriniz
+
+Please Enter Data
+""")
+
+data = input("Veri / Data: ")
+
+image = qrcode.make(data)
+
+
+image.save('qrkod.png')
+image.show('qrkod.png')
+
